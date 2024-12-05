b4f59e6ff8b10e7b40e5120f491fe8f4326c9e74
Cole Goodhart <colegoodhart@gmail.com>
2024-12-05 16:42:28 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include <bitset>
+#include <string>
+#include <stdexcept>
+
+// Function to calculate the Hamming distance between two strings
+int hamming_distance(const std::string& str1, const std::string& str2) {
+    if (str1.size() != str2.size()) {
+        throw std::invalid_argument("Strings must be of equal length.");
+    }
+
+    int distance = 0;
+    for (size_t i = 0; i < str1.size(); i++) {
+        distance += std::bitset<8>(str1[i] ^ str2[i]).count();
+    }
+    return distance;
+}
+
+// Test cases for Hamming distance
+TEST(HammingDistanceTest, EqualStrings) {
+    EXPECT_EQ(hamming_distance("this is a test", "wokka wokka!!!"), 37);
+}
+
+TEST(HammingDistanceTest, IdenticalStrings) {
+    EXPECT_EQ(hamming_distance("hello", "hello"), 0);
+}
+
+TEST(HammingDistanceTest, DifferentStrings) {
+    EXPECT_EQ(hamming_distance("10101", "10011"), 3);
+}
+
+TEST(HammingDistanceTest, ThrowsOnDifferentLengths) {
+    EXPECT_THROW(hamming_distance("short", "longer string"), std::invalid_argument);
+}
+
+// Main function to run tests
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
